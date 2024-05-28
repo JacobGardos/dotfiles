2948509056af384a85e8e8b7d708d89bc3ae4ba8
Jacob Gardos <49768172+JacobGardos@users.noreply.github.com>
2024-05-28 09:05:23 +0000

    Symlink zshrc file

--- bin/install.sh
+++ bin/install.sh
@@ -1,3 +1,9 @@
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Delete the existing zshrc file
+rm ~/.zshrc -f 
+
+# Create a symlink to the zshrc file
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
